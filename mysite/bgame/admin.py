113473a5bdd628708ccf88d7a4a2bf7362f7d3a6
Koblaid <koblaid@googlemail.com>
2012-05-15 15:05:16 +0200

    admin.py: Minor refactoring

--- mysite/bgame/admin.py
+++ mysite/bgame/admin.py
@@ -1,27 +1,29 @@
-from mysite.bgame import models
 from django.contrib import admin
 
+from mysite.bgame import models as M
+
+
 class BuildingType_Resource_Inline(admin.TabularInline):
-    model = models.BuildingType_Resource
+    model = M.BuildingType_Resource
     extra = 1
-    
+
 class BuildingTypeAdmin(admin.ModelAdmin):
     inlines = [BuildingType_Resource_Inline]
 
 
 
 class Resource_Inline(admin.TabularInline):
-    model = models.Player_Resource
+    model = M.Player_Resource
     extra = 1
-    
+
 class Building_Inline(admin.TabularInline):
-    model = models.Player_Building
+    model = M.Player_Building
     extra = 1
-    
+
 class PlayerAdmin(admin.ModelAdmin):
     inlines = [Resource_Inline, Building_Inline]
 
-    
-admin.site.register(models.ResourceType)
-admin.site.register(models.BuildingType, BuildingTypeAdmin)
-admin.site.register(models.Player, PlayerAdmin)
+
+admin.site.register(M.ResourceType)
+admin.site.register(M.BuildingType, BuildingTypeAdmin)
+admin.site.register(M.Player, PlayerAdmin)
