2d74a692b8d96f8d1a229a7dbc074c7df2e1202a
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-06-19 11:26:39 +0200

    settings.py: Print the chosen db path

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,10 +19,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+db_path = os.environ.get('SQLITE_PATH') or 'testdb.sqlite'
+print('Using db %s' % repr(db_path))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.environ.get('SQLITE_PATH') or 'testdb.sqlite',    # Or path to database file if using sqlite3.
+        'NAME': db_path,                 # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
