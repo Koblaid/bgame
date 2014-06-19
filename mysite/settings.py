abead75249e37772b8e7ce17724bb49d2674f7d2
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-06-19 14:19:26 +0200

    settings.py: Added settings for static urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.environ.get('STATIC_ROOT')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
